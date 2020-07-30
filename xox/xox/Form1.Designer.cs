cd8a851ffab74df6fbfd5660848952b4751e96a4
keremio <kyakutlu1003@gmail.com>
2020-07-30 12:49:48 +0300

    Add files via upload

---
+++ xox/xox/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace xox
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(78, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(29, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(113, 61);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(29, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(148, 61);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(29, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(78, 87);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(29, 20);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(113, 87);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(29, 20);
+            this.textBox5.TabIndex = 4;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(148, 87);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(29, 20);
+            this.textBox6.TabIndex = 3;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(78, 113);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(29, 20);
+            this.textBox7.TabIndex = 8;
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(113, 113);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(29, 20);
+            this.textBox8.TabIndex = 7;
+            this.textBox8.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(148, 113);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(29, 20);
+            this.textBox9.TabIndex = 6;
+            this.textBox9.TextChanged += new System.EventHandler(this.textBox9_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(110, 173);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(90, 147);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "press";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(253, 220);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ xox/xox/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace xox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "X" && textBox1.Text == textBox2.Text && textBox2.Text == textBox3.Text && textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "")
+                label1.Text = "X WON";
+            if (textBox1.Text == "X" && textBox1.Text == textBox4.Text && textBox4.Text == textBox7.Text && textBox1.Text != "" && textBox4.Text != "" && textBox7.Text != "")
+                label1.Text = "X WON";
+            if (textBox1.Text == "X" && textBox1.Text == textBox5.Text && textBox5.Text == textBox9.Text && textBox1.Text != "" && textBox5.Text != "" && textBox9.Text != "")
+                label1.Text = "X WON";
+
+            if (textBox1.Text == "O" && textBox1.Text == textBox2.Text && textBox2.Text == textBox3.Text && textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "")
+                label1.Text = "O WON";
+            if (textBox1.Text == "O" && textBox1.Text == textBox4.Text && textBox4.Text == textBox7.Text && textBox1.Text != "" && textBox4.Text != "" && textBox7.Text != "")
+                label1.Text = "O WON";
+            if (textBox1.Text == "O" && textBox1.Text == textBox5.Text && textBox5.Text == textBox9.Text && textBox1.Text != "" && textBox5.Text != "" && textBox9.Text != "")
+                label1.Text = "O WON";
+
+            if (textBox1.Text == "")
+                label1.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+            textBox7.Text = "";
+            textBox8.Text = "";
+            textBox9.Text = "";
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "X" && textBox1.Text == textBox2.Text && textBox2.Text == textBox3.Text && textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "")
+                label1.Text = "X WON";
+            if (textBox2.Text == "X" && textBox2.Text == textBox5.Text && textBox5.Text == textBox8.Text && textBox2.Text != "" && textBox5.Text != "" && textBox8.Text != "")
+                label1.Text = "X WON";
+
+            if (textBox1.Text == "O" && textBox1.Text == textBox2.Text && textBox2.Text == textBox3.Text && textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "")
+                label1.Text = "O WON";
+            if (textBox2.Text == "O" && textBox2.Text == textBox5.Text && textBox5.Text == textBox8.Text && textBox2.Text != "" && textBox5.Text != "" && textBox8.Text != "")
+                label1.Text = "O WON";
+
+            if (textBox2.Text == "")
+                label1.Text = "";
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "X" && textBox1.Text == textBox2.Text && textBox2.Text == textBox3.Text && textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "")
+                label1.Text = "X WON";
+            if (textBox3.Text == "X" && textBox3.Text == textBox6.Text && textBox6.Text == textBox9.Text && textBox3.Text != "" && textBox6.Text != "" && textBox9.Text != "")
+                label1.Text = "X WON";
+            if (textBox3.Text == "X" && textBox3.Text == textBox5.Text && textBox5.Text == textBox7.Text && textBox3.Text != "" && textBox5.Text != "" && textBox7.Text != "")
+                label1.Text = "X WON";
+
+            if (textBox1.Text == "O" && textBox1.Text == textBox2.Text && textBox2.Text == textBox3.Text && textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "")
+                label1.Text = "O WON";
+            if (textBox3.Text == "O" && textBox3.Text == textBox6.Text && textBox6.Text == textBox9.Text && textBox3.Text != "" && textBox6.Text != "" && textBox9.Text != "")
+                label1.Text = "O WON";
+            if (textBox3.Text == "O" && textBox3.Text == textBox5.Text && textBox5.Text == textBox7.Text && textBox3.Text != "" && textBox5.Text != "" && textBox7.Text != "")
+                label1.Text = "O WON";
+
+            if (textBox3.Text == "")
+                label1.Text = "";
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox4.Text == "X" && textBox4.Text == textBox5.Text && textBox5.Text == textBox6.Text && textBox4.Text != "" && textBox5.Text != "" && textBox6.Text != "")
+                label1.Text = "X WON";
+            if (textBox1.Text == "X" && textBox1.Text == textBox4.Text && textBox4.Text == textBox7.Text && textBox1.Text != "" && textBox4.Text != "" && textBox7.Text != "")
+                label1.Text = "X WON";
+
+            if (textBox4.Text == "O" && textBox4.Text == textBox5.Text && textBox5.Text == textBox6.Text && textBox4.Text != "" && textBox5.Text != "" && textBox6.Text != "")
+                label1.Text = "O WON";
+            if (textBox1.Text == "O" && textBox1.Text == textBox4.Text && textBox4.Text == textBox7.Text && textBox1.Text != "" && textBox4.Text != "" && textBox7.Text != "")
+                label1.Text = "O WON";
+
+            if (textBox4.Text == "")
+                label1.Text = "";
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox4.Text == "X" && textBox4.Text == textBox5.Text && textBox5.Text == textBox6.Text && textBox4.Text != "" && textBox5.Text != "" && textBox6.Text != "")
+                label1.Text = "X WON";
+            if (textBox2.Text == "X" && textBox2.Text == textBox5.Text && textBox5.Text == textBox8.Text && textBox2.Text != "" && textBox5.Text != "" && textBox8.Text != "")
+                label1.Text = "X WON";
+            if (textBox3.Text == "X" && textBox3.Text == textBox5.Text && textBox5.Text == textBox7.Text && textBox3.Text != "" && textBox5.Text != "" && textBox7.Text != "")
+                label1.Text = "X WON";
+
+            if (textBox4.Text == "O" && textBox4.Text == textBox5.Text && textBox5.Text == textBox6.Text && textBox4.Text != "" && textBox5.Text != "" && textBox6.Text != "")
+                label1.Text = "O WON";
+            if (textBox2.Text == "O" && textBox2.Text == textBox5.Text && textBox5.Text == textBox8.Text && textBox2.Text != "" && textBox5.Text != "" && textBox8.Text != "")
+                label1.Text = "O WON";
+            if (textBox3.Text == "O" && textBox3.Text == textBox5.Text && textBox5.Text == textBox7.Text && textBox3.Text != "" && textBox5.Text != "" && textBox7.Text != "")
+                label1.Text = "O WON";
+
+            if (textBox5.Text == "")
+                label1.Text = "";
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox3.Text == "X" && textBox3.Text == textBox6.Text && textBox6.Text == textBox9.Text && textBox3.Text != "" && textBox6.Text != "" && textBox9.Text != "")
+                label1.Text = "X WON";
+            if (textBox4.Text == "X" && textBox4.Text == textBox5.Text && textBox5.Text == textBox6.Text && textBox4.Text != "" && textBox5.Text != "" && textBox6.Text != "")
+                label1.Text = "X WON";
+
+            if (textBox3.Text == "O" && textBox3.Text == textBox6.Text && textBox6.Text == textBox9.Text && textBox3.Text != "" && textBox6.Text != "" && textBox9.Text != "")
+                label1.Text = "O WON";
+            if (textBox4.Text == "O" && textBox4.Text == textBox5.Text && textBox5.Text == textBox6.Text && textBox4.Text != "" && textBox5.Text != "" && textBox6.Text != "")
+                label1.Text = "O WON";
+
+            if (textBox6.Text == "")
+                label1.Text = "";
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox7.Text == "X" && textBox7.Text == textBox8.Text && textBox8.Text == textBox9.Text && textBox7.Text != "" && textBox8.Text != "" && textBox9.Text != "")
+                label1.Text = "X WON";
+            if (textBox1.Text == "X" && textBox1.Text == textBox4.Text && textBox4.Text == textBox7.Text && textBox1.Text != "" && textBox4.Text != "" && textBox7.Text != "")
+                label1.Text = "X WON";
+            if (textBox3.Text == "X" && textBox3.Text == textBox5.Text && textBox5.Text == textBox7.Text && textBox3.Text != "" && textBox5.Text != "" && textBox7.Text != "")
+                label1.Text = "X WON";
+
+            if (textBox7.Text == "O" && textBox7.Text == textBox8.Text && textBox8.Text == textBox9.Text && textBox7.Text != "" && textBox8.Text != "" && textBox9.Text != "")
+                label1.Text = "O WON";
+            if (textBox1.Text == "O" && textBox1.Text == textBox4.Text && textBox4.Text == textBox7.Text && textBox1.Text != "" && textBox4.Text != "" && textBox7.Text != "")
+                label1.Text = "O WON";
+            if (textBox3.Text == "O" && textBox3.Text == textBox5.Text && textBox5.Text == textBox7.Text && textBox3.Text != "" && textBox5.Text != "" && textBox7.Text != "")
+                label1.Text = "O WON";
+
+            if (textBox7.Text == "")
+                label1.Text = "";
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox7.Text == "X" && textBox7.Text == textBox8.Text && textBox8.Text == textBox9.Text && textBox7.Text != "" && textBox8.Text != "" && textBox9.Text != "")
+                label1.Text = "X WON";
+            if (textBox2.Text == "X" && textBox2.Text == textBox5.Text && textBox5.Text == textBox8.Text && textBox2.Text != "" && textBox5.Text != "" && textBox8.Text != "")
+                label1.Text = "X WON";
+
+            if (textBox7.Text == "O" && textBox7.Text == textBox8.Text && textBox8.Text == textBox9.Text && textBox7.Text != "" && textBox8.Text != "" && textBox9.Text != "")
+                label1.Text = "O WON";
+            if (textBox2.Text == "O" && textBox2.Text == textBox5.Text && textBox5.Text == textBox8.Text && textBox2.Text != "" && textBox5.Text != "" && textBox8.Text != "")
+                label1.Text = "O WON";
+
+            if (textBox8.Text == "")
+                label1.Text = "";
+        }
+
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox7.Text == "X" && textBox7.Text == textBox8.Text && textBox8.Text == textBox9.Text && textBox7.Text != "" && textBox8.Text != "" && textBox9.Text != "")
+                label1.Text = "X WON";
+            if (textBox3.Text == "X" && textBox3.Text == textBox6.Text && textBox6.Text == textBox9.Text && textBox3.Text != "" && textBox6.Text != "" && textBox9.Text != "")
+                label1.Text = "X WON";
+            if (textBox1.Text == "O" && textBox1.Text == textBox5.Text && textBox5.Text == textBox9.Text && textBox1.Text != "" && textBox5.Text != "" && textBox9.Text != "")
+                label1.Text = "O WON";
+
+            if (textBox9.Text == "")
+                label1.Text = "";
+        }
+    }
+}
